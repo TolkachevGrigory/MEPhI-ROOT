3e3a553d468da74160e165cd65439d49a8c118ab
TolkachevGrigory <55763195+TolkachevGrigory@users.noreply.github.com>
2019-11-08 22:59:39 +0300

    Update minus_beackground_signal_nomel.C

--- minus_beackground_signal_nomel.C
+++ minus_beackground_signal_nomel.C
@@ -1,7 +1,7 @@
 //электронный кана сигнал/шум с татистическую ошибку
 void minus_beackground_signal_nomel()
 {
-  TFile* f1= new TFile("/Users/grigorijtolkacev/Desktop/ATLAS/SW/mon_el/signal/minus.1.SM_WLepton.root", "READ");
+ TFile* f1= new TFile("/Users/grigorijtolkacev/Desktop/ATLAS/SW/mon_el/signal/minus.1.SM_WLepton.root", "READ");
   TFile* f2= new TFile("/Users/grigorijtolkacev/Desktop/ATLAS/SW/mon_el/background/minus.6.SM_WLepton.root", "READ");
 
   auto taunu = (TTree*)f1->Get("NOMINAL");
@@ -10,7 +10,7 @@ void minus_beackground_signal_nomel()
     //histo for comparing signal - beackground
   TH1F *h_tau_pt = new TH1F("tau_pt", "#tau^{#minus} ;P_{T} [GeV];Normalized Units", 150, 20, 90);
   TH1F *h_tau_phi_minus = new TH1F("tau_phi_minus", "#tau^{#minus};d#varphi (l-E^{miss}_{T}) ;Normalized Units", 150, -4, 4);
-  TH1F *h_tau_phi = new TH1F("tau_phi", "#tau^{#minus};#varph ;Normalized Units", 150, -5, 5);
+  TH1F *h_tau_phi = new TH1F("tau_phi", "#tau^{#minus};#varphi ;Normalized Units", 150, -5, 5);
   TH1F *h_taumet_phi = new TH1F("taumet_phi", " #bar{#nu}_{#tau};#varphi ;Normalized Units", 150, -5, 5);
   TH1F *h_taumet_energy = new TH1F("taumet_energy", "#bar{#nu}_{#tau};E [GeV];Normalized Units", 150, 20, 100);
   TH1F *h_taunu_mt = new TH1F("tau_mt", "W^{#minus} #rightarrow #tau^{#minus} #plus #bar{#nu}_{#tau};m_{T} [GeV];Normalized Units", 150, 35, 160);
@@ -20,7 +20,7 @@ void minus_beackground_signal_nomel()
     
   TH1F *h_mu_pt = new TH1F("e_pt", "e^{#minus} ;P_{T} [GeV];Normalized Units", 150, 20, 90);
   TH1F *h_mu_phi_minus = new TH1F("e_phi", "e^{#minus};d#varphi (l-E^{miss}_{T}) ;Normalized Units", 150, -4, 4);
-  TH1F *h_mu_phi = new TH1F("e_phi", "e^{#minus};#varph ;Normalized Units", 150, -5, 5);
+  TH1F *h_mu_phi = new TH1F("e_phi", "e^{#minus};#varphi ;Normalized Units", 150, -5, 5);
   TH1F *h_mumet_phi = new TH1F("emet_phi", " #bar{#nu}_{e};#varphi ;Normalized Units", 150, -4, 4);
   TH1F *h_mumet_energy = new TH1F("e_energy", "#bar{#nu}_{e};E [GeV];Normalized Units", 150, 20, 120);
   TH1F *h_munu_mt = new TH1F("e_mt", "W^{#minus} #rightarrow e^{#minus} #plus #bar{#nu}_{e};m_{T} [GeV];Normalized Units", 150, 35, 160);
@@ -168,7 +168,7 @@ void minus_beackground_signal_nomel()
           h_mu_phi->Fill(mu_phi);
           h_mumet_phi->Fill(mumet_phi);
           
-          if ( !(fabs(phi_mu)<3.14 && fabs(phi_mu)>0))continue;
+        if ( !(fabs(phi_mu)<3.14 && fabs(phi_mu)>0))continue;
           
           
           h_mu_pt->Fill(mu_pt);
@@ -190,45 +190,62 @@ void minus_beackground_signal_nomel()
     h_tau_pt->Scale(norm/h_tau_pt->Integral(), "width");
     
     h_tau_pt->SetLineColor(kBlue+3);
-    h_tau_pt->Sumw2();
+    //h_tau_pt->Sumw2();
     h_tau_pt-> SetMarkerStyle (21);
     h_tau_pt-> SetMarkerSize (0.2);
     h_tau_pt->SetFillStyle(4050);
     h_tau_pt->SetFillColor(kBlue-7);
-    h_tau_pt->Draw("E HIST");
+    h_tau_pt->Draw("HIST");
+    h_tau_pt->GetYaxis()->SetRangeUser(0., 0.12);
+    h_tau_pt->Draw("E same");
 
     
     h_mu_pt->SetLineColor(kRed+2);
-    h_mu_pt->Sumw2();
+  //  h_mu_pt->Sumw2();
     h_mu_pt-> SetMarkerStyle (21);
     h_mu_pt-> SetMarkerSize (0.2);
     h_mu_pt->SetFillColor(kRed);
     h_mu_pt->SetFillStyle(3354);
-     h_mu_pt->Draw("HIST E same ");
+    h_mu_pt->Draw("HIST  E same");
+    
+    auto legend1 = new TLegend(0.89,0.88,0.77,0.67);
+ //   legend1->SetHeader("The Legend Title","C"); // option "C" allows to center the header
+    legend1->AddEntry( h_tau_pt,"#tau^{#minus} ","f");
+    legend1->AddEntry( h_mu_pt,"e^{#minus} ","f");
+    legend1->Draw();
+
+    
  
-    gPad->BuildLegend(0.9,0.9,0.8,0.7);
+    
       
     c2->cd();
     h_mu_phi_minus->Scale(norm/h_mu_phi_minus->Integral(), "width");
     h_tau_phi_minus->Scale(norm/h_tau_phi_minus->Integral(), "width");
     
     h_mu_phi_minus->SetLineColor(kBlue+3);
-    h_mu_phi_minus->Sumw2();
+   // h_mu_phi_minus->Sumw2();
     h_mu_phi_minus-> SetMarkerStyle (21);
     h_mu_phi_minus-> SetMarkerSize (0.2);
     h_mu_phi_minus->SetFillStyle(4050);
     h_mu_phi_minus->SetFillColor(kBlue-7);
-    h_mu_phi_minus->Draw("E HIST");
+    h_mu_phi_minus->Draw("HIST");
+    h_mu_phi_minus->Draw("E same ");
 
        
     h_tau_phi_minus->SetLineColor(kRed+2);
-    h_tau_phi_minus->Sumw2();
+   // h_tau_phi_minus->Sumw2();
     h_tau_phi_minus-> SetMarkerStyle (21);
     h_tau_phi_minus-> SetMarkerSize (0.2);
     h_tau_phi_minus->SetFillColor(kRed);
     h_tau_phi_minus->SetFillStyle(3354);
     h_tau_phi_minus->Draw("HIST E same ");
-    gPad->BuildLegend(0.9,0.9,0.8,0.7);
+    
+    
+    auto legend2 = new TLegend(0.89,0.88,0.77,0.67);
+    
+       legend2->AddEntry( h_tau_pt,"#tau^{#minus} ","f");
+       legend2->AddEntry( h_mu_pt,"e^{#minus} ","f");
+       legend2->Draw();
     
 
     c3->cd();
@@ -236,22 +253,27 @@ void minus_beackground_signal_nomel()
     h_taumet_energy->Scale(norm/h_taumet_energy->Integral(), "width");
     
     h_taumet_energy->SetLineColor(kBlue+3);
-    h_taumet_energy->Sumw2();
+   // h_taumet_energy->Sumw2();
     h_taumet_energy-> SetMarkerStyle (21);
     h_taumet_energy-> SetMarkerSize (0.2);
     h_taumet_energy->SetFillStyle(4050);
     h_taumet_energy->SetFillColor(kBlue-7);
-    h_taumet_energy->Draw("E HIST");
-
+    h_taumet_energy->GetYaxis()->SetRangeUser(0., 0.070);
+    h_taumet_energy->Draw("HIST");
+    h_taumet_energy->Draw("E same");
     
     h_mumet_energy->SetLineColor(kRed+2);
-    h_mumet_energy->Sumw2();
+   // h_mumet_energy->Sumw2();
     h_mumet_energy->SetMarkerStyle (21);
     h_mumet_energy-> SetMarkerSize (0.2);
     h_mumet_energy->SetFillColor(kRed);
     h_mumet_energy->SetFillStyle(3354);
     h_mumet_energy->Draw("HIST E same ");
-    gPad->BuildLegend(0.9,0.9,0.8,0.7);
+    
+    auto legend3 = new TLegend(0.89,0.88,0.77,0.67);
+       legend3->AddEntry( h_taumet_energy," #bar{#nu}_{#tau}","f");
+       legend3->AddEntry( h_mumet_energy," #bar{#nu}_{e}","f");
+       legend3->Draw();
     
     c4->cd();
     h_taunu_mt->Scale(norm/h_taunu_mt->Integral(), "width");
@@ -261,23 +283,30 @@ void minus_beackground_signal_nomel()
     
     
     h_taunu_mt->SetLineColor(kBlue+3);
-    h_taunu_mt->Sumw2();
+   // h_taunu_mt->Sumw2();
     h_taunu_mt-> SetMarkerStyle (21);
     h_taunu_mt-> SetMarkerSize (0.2);
     h_taunu_mt->SetFillStyle(4050);
     h_taunu_mt->SetFillColor(kBlue-7);
-    h_taunu_mt->Draw("E HIST");
+    h_taunu_mt->GetYaxis()->SetRangeUser(0., 0.050);
+    h_taunu_mt->Draw("HIST");
+    h_taunu_mt->Draw("E same");
 
     
     h_munu_mt->SetLineColor(kRed+2);
-    h_munu_mt->Sumw2();
+   // h_munu_mt->Sumw2();
     h_munu_mt->SetMarkerStyle (21);
     h_munu_mt-> SetMarkerSize (0.2);
     h_munu_mt->SetFillColor(kRed);
     h_munu_mt->SetFillStyle(3354);
     h_munu_mt->Draw("HIST E same ");
 
-    gPad->BuildLegend(0.9,0.9,0.6,0.50);
+   // gPad->BuildLegend(0.9,0.9,0.6,0.50);
+    auto legend4 = new TLegend(0.9,0.9,0.6,0.50);
+    legend4->AddEntry( h_taunu_mt,"W^{#minus} #rightarrow #tau^{#minus} #plus #bar{#nu}_{#tau} ","f");
+    legend4->AddEntry( h_munu_mt,"W^{#minus} #rightarrow e^{#minus} #plus #bar{#nu}_{e} ","f");
+    legend4->Draw();
+        
      
     c5->cd();
     
@@ -287,16 +316,18 @@ void minus_beackground_signal_nomel()
     
     
     h_tau_phi->SetLineColor(kBlue+3);
-     h_tau_phi->Sumw2();
+   //  h_tau_phi->Sumw2();
      h_tau_phi-> SetMarkerStyle (21);
      h_tau_phi-> SetMarkerSize (0.2);
      h_tau_phi->SetFillStyle(4050);
      h_tau_phi->SetFillColor(kBlue-7);
-     h_tau_phi->Draw("E HIST");
+     h_tau_phi->GetYaxis()->SetRangeUser(0., 0.25);
+     h_tau_phi->Draw("HIST");
+     h_tau_phi->Draw("E same");
 
     
     h_mu_phi->SetLineColor(kRed+2);
-    h_mu_phi->Sumw2();
+   // h_mu_phi->Sumw2();
     h_mu_phi->SetMarkerStyle (21);
     h_mu_phi-> SetMarkerSize (0.2);
     h_mu_phi->SetFillColor(kRed);
@@ -304,7 +335,10 @@ void minus_beackground_signal_nomel()
     h_mu_phi->Draw("HIST E same ");
    
     
-    gPad->BuildLegend(0.9,0.9,0.8,0.7);
+    auto legend5 = new TLegend(0.89,0.88,0.77,0.67);
+    legend5->AddEntry( h_tau_phi,"#tau^{#minus} ","f");
+    legend5->AddEntry( h_mu_phi,"e^{#minus} ","f");
+    legend5->Draw();
         
     c6->cd();
     h_taumet_phi->Scale(norm/h_taumet_phi->Integral(), "width");
@@ -313,39 +347,48 @@ void minus_beackground_signal_nomel()
     
     
     h_taumet_phi->SetLineColor(kBlue+3);
-     h_taumet_phi->Sumw2();
+   //  h_taumet_phi->Sumw2();
      h_taumet_phi-> SetMarkerStyle (21);
      h_taumet_phi-> SetMarkerSize (0.2);
      h_taumet_phi->SetFillStyle(4050);
      h_taumet_phi->SetFillColor(kBlue-7);
-     h_taumet_phi->Draw("E HIST");
+     h_taumet_phi->GetYaxis()->SetRangeUser(0., 0.24);
+     h_taumet_phi->Draw("HIST");
+     h_taumet_phi->Draw("E same");
+
 
     
     h_mumet_phi->SetLineColor(kRed+2);
-    h_mumet_phi->Sumw2();
+   // h_mumet_phi->Sumw2();
     h_mumet_phi->SetMarkerStyle (21);
     h_mumet_phi-> SetMarkerSize (0.2);
     h_mumet_phi->SetFillColor(kRed);
     h_mumet_phi->SetFillStyle(3354);
     h_mumet_phi->Draw("HIST E same ");
     
-    gPad->BuildLegend(0.9,0.9,0.8,0.7);
+    auto legend6 = new TLegend(0.89,0.88,0.77,0.67);
+    legend6->AddEntry( h_taumet_phi," #bar{#nu}_{#tau}","f");
+    legend6->AddEntry( h_mumet_phi," #bar{#nu}_{e}","f");
+    legend6->Draw();
     
     c7->cd();
     h_tau_eta->Scale(norm/h_tau_eta->Integral(), "width");
     h_mu_eta->Scale(norm/h_mu_eta->Integral(), "width");
     
      h_tau_eta->SetLineColor(kBlue+3);
-     h_tau_eta->Sumw2();
+   //  h_tau_eta->Sumw2();
      h_tau_eta-> SetMarkerStyle (21);
      h_tau_eta-> SetMarkerSize (0.2);
      h_tau_eta->SetFillStyle(4050);
      h_tau_eta->SetFillColor(kBlue-7);
-     h_tau_eta->Draw("E HIST");
+     h_tau_eta->GetYaxis()->SetRangeUser(0., 0.43);
+     h_tau_eta->Draw("HIST");
+     h_tau_eta->Draw("E same");
+
 
     
      h_mu_eta->SetLineColor(kRed+2);
-     h_mu_eta->Sumw2();
+ //    h_mu_eta->Sumw2();
      h_mu_eta->SetMarkerStyle (21);
      h_mu_eta-> SetMarkerSize (0.2);
      h_mu_eta->SetFillColor(kRed);
@@ -353,7 +396,11 @@ void minus_beackground_signal_nomel()
      h_mu_eta->Draw("HIST E same ");
     
     
-    gPad->BuildLegend(0.9,0.9,0.8,0.7);
+    auto legend7 = new TLegend(0.89,0.88,0.77,0.67);
+       legend7->AddEntry( h_tau_eta,"#tau^{#minus} ","f");
+       legend7->AddEntry( h_mu_eta,"e^{#minus} ","f");
+       legend7->Draw();
+
 
     
     c1->SaveAs("/Users/grigorijtolkacev/Desktop/ATLAS/SW/lep_eandtau_minus_pt.pdf");
